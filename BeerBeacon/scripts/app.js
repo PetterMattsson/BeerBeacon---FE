a3658c8c9c038fd3bd5c9d8ac4d5fb0244d257e3
Petter Mattsson <mobilpetter@gmail.com>
2017-08-11 19:31:59 +0200

    Flyttat kollen för FB inlogg

--- BeerBeacon/scripts/app.js
+++ BeerBeacon/scripts/app.js
@@ -149,14 +149,7 @@ var globalApp = (function () {
    * Methods to handle FB login
    *
    ****************************************************************************/
-  app.testAPI = function () {
-      console.log('Welcome!  Fetching your information.... ');
-      FB.api('/me', function (response) {
-          console.log('Successful login for: ' + response.name);
-          document.getElementById('status').innerHTML =
-              'Thanks for logging in, ' + response.name + '!';
-      });
-  };
+  
 
     /*
 
